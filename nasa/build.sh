0e40e62ce4d37b5194417558343e9a0f09418c53
Ryan Burns <rtburns@jpl.nasa.gov>
2025-09-09 15:42:33 -0700

    Simplify algorithm build process

--- nasa/build.sh
+++ nasa/build.sh
@@ -4,14 +4,5 @@ set -Eeuo pipefail
 
 basedir=$(dirname "$(dirname "$(readlink -f "$0")")")
 
-# Build *without* development dependencies.  Unfortunately, DPS does not allow
-# us to pass arguments to an algorithm's "build" script, so this script is
-# required as a wrapper for DPS to call.
-
-# When we build the algorithm container, we need to build the
-# custom environment into the algorithm container.
-# Later, when we use the run-dps.sh script to run in DPS,
-# make sure that script uses the same conda environment.
-# Note: This is where the name of the conda environment gets set.
-# (It is not set per the `name` in the environment.yml.)
-SAR2D2_ENV=sar2-d2 "${basedir}"/bin/conda/install.sh "${basedir}/environment.yml"
+conda env update --solver libmamba \
+    --name sar2-d2 --file "${basedir}/environment.yml"
